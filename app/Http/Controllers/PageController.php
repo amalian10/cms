2f0d2d89d7a1c757e423db24fe67ecbd7fc83c56
amalian10 <2041720160@student.polinema.ac.id>
2022-02-22 19:28:04 +0700

    Practicum 2 (1-4)

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PageController extends Controller
+{
+    public function index(){
+        return "Hi! Welcome To Laravel";
+    }
+    public function about(){
+        return "Name : Amalia Nuraini - NIM : 2041720160 - Class : TI 2I";
+    }
+    public function articles($id){
+        return "Article Pages with Id" .$id;
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PageController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,12 +14,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    echo 'Hi! Welcome To Laravel';
-});
-Route::get('/about', function () {
-    return 'Name : Amalia Nuraini - NIM : 2041720160 - Class : TI 2I';
-});
-Route::get('/articles/{id}', function ($id) {
-    return 'This is Article Pages with ID: ' .$id;
-});
+// practicum 1
+
+// Route::get('/', function () {
+//     echo 'Hi! Welcome To Laravel';
+// });
+// Route::get('/about', function () {
+//     return 'Name : Amalia Nuraini - NIM : 2041720160 - Class : TI 2I';
+// });
+// Route::get('/articles/{id}', function ($id) {
+//     return 'This is Article Pages with ID: ' .$id;
+// });
+
+// practicum 2
+
+Route::get('/', [PageController::class, 'index']);
+Route::get('/about', [PageController::class, 'about']);
+Route::get('/articles/{id}', [PageController::class, 'articles']);
